ff5412cafbee2a2e97689bc189a66d5421d96a3d
heisenshark <46131664+heisenshark@users.noreply.github.com>
2022-12-09 01:36:10 +0000

    random function, random renamed to randomInt

--- src/functions/number.ts
+++ src/functions/number.ts
@@ -3,11 +3,19 @@ export function compactNumber(number) {
     return Intl.NumberFormat('en', { notation: 'compact' }).format(number)
 }
 
+export function randomInt(min = 1, max) {
+    if (max == null) {
+        max = min
+        min = 0
+    }
+
+    return Math.floor(Math.random() * (max - min + 1)) + min
+}
+
 export function random(min = 1, max) {
     if (max == null) {
         max = min
         min = 0
     }
-    
-    return Math.floor(Math.random() * (max - min + 1)) + min
+    return Math.random() * (max - min + 1) + min
 }
